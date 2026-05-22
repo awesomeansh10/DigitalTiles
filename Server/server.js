e5c45678d4fde192aabadc1698b34ed7740200cf
Ansh <awesomeansh10@gmail.com>
2026-05-22 19:21:03 +0100

    new tiles will have relevant links

--- Server/server.js
+++ Server/server.js
@@ -12,7 +12,8 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 let graphState = {
     nodes: {},
-    edges: []
+    edges: [],
+    tileConnections: [] // Keeps track of tile-to-tile connections
 };
 
 io.on('connection', (socket) => {
@@ -23,6 +24,27 @@ io.on('connection', (socket) => {
     socket.on('add_node', (nodeData) => {
         graphState.nodes[nodeData.id] = nodeData;
         io.emit('node_added', nodeData);
+        
+        // Automatically link the new node to other tiles if a tile connection exists
+        graphState.tileConnections.forEach(conn => {
+            if (conn.source === nodeData.deviceId) {
+                // The new node belongs to a source tile, connect it to all target nodes
+                const targetNodes = Object.values(graphState.nodes).filter(n => n.deviceId === conn.target);
+                targetNodes.forEach(targetNode => {
+                    const edgeData = { source: nodeData.id, target: targetNode.id };
+                    graphState.edges.push(edgeData);
+                    io.emit('edge_added', edgeData);
+                });
+            } else if (conn.target === nodeData.deviceId) {
+                // The new node belongs to a target tile, connect all source nodes to it
+                const sourceNodes = Object.values(graphState.nodes).filter(n => n.deviceId === conn.source);
+                sourceNodes.forEach(sourceNode => {
+                    const edgeData = { source: sourceNode.id, target: nodeData.id };
+                    graphState.edges.push(edgeData);
+                    io.emit('edge_added', edgeData);
+                });
+            }
+        });
     });
 
     // 2. Live-update the drawing
@@ -51,14 +73,29 @@ io.on('connection', (socket) => {
 
     // 5. Connect all nodes from one tile to another
     socket.on('connect_tiles', (data) => {
+        // Record the connection between the tiles if it hasn't been recorded yet
+        const connectionExists = graphState.tileConnections.some(
+            conn => conn.source === data.source && conn.target === data.target
+        );
+        
+        if (!connectionExists) {
+            graphState.tileConnections.push({ source: data.source, target: data.target });
+        }
+
         const sourceNodes = Object.values(graphState.nodes).filter(n => n.deviceId === data.source);
         const targetNodes = Object.values(graphState.nodes).filter(n => n.deviceId === data.target);
         
         sourceNodes.forEach(sourceNode => {
             targetNodes.forEach(targetNode => {
-                const edgeData = { source: sourceNode.id, target: targetNode.id };
-                graphState.edges.push(edgeData);
-                io.emit('edge_added', edgeData);
+                // Only create the edge if it doesn't already exist to avoid duplicates
+                const edgeExists = graphState.edges.some(
+                    e => e.source === sourceNode.id && e.target === targetNode.id
+                );
+                if (!edgeExists) {
+                    const edgeData = { source: sourceNode.id, target: targetNode.id };
+                    graphState.edges.push(edgeData);
+                    io.emit('edge_added', edgeData);
+                }
             });
         });
     });
