f6f695d98af6f766abe03d7ca74fa500613ce3b2
Ansh <awesomeansh10@gmail.com>
2026-06-07 03:11:25 +0100

    pin cahnge maybe

--- Server/client.py
+++ Server/client.py
@@ -28,8 +28,8 @@ sio = socketio.Client()
 BAUD_RATE = 9600
 PORTS = {
     'left':    {'tx': 20,  'rx': 21,  'connected_to': None, 'cb': None},
-    'right':  {'tx': 12, 'rx': 7, 'connected_to': None, 'cb': None},
-    'bottom': {'tx': 8, 'rx': 25, 'connected_to': None, 'cb': None},
+    'bottom':  {'tx': 12, 'rx': 7, 'connected_to': None, 'cb': None},
+    'right': {'tx': 25, 'rx': 8, 'connected_to': None, 'cb': None},
     'top':   {'tx': 24, 'rx': 23, 'connected_to': None, 'cb': None}
 }
 
